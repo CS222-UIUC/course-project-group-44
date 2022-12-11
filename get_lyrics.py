69dc53f1de1870e9b5bda2f73ef6588bd8d0eb50
SumayT9 <sthak99@gmail.com>
2022-12-10 20:33:53 -0600

    Fixed pylint issues

--- get_lyrics.py
+++ get_lyrics.py
@@ -1,15 +1,15 @@
 """
 Code to get all song lyrics by an artist
 """
-from time import sleep
 from urllib.request import urlopen
-from urllib.parse import quote
+import os
+import re
 import requests
 from bs4 import BeautifulSoup
-from const import KEY, L_COM_KEY   # pylint: disable=import-error
-import lyricsgenius
-import re
-import os
+import lyricsgenius # pylint: disable=import-error
+from const import KEY, L_COM_KEY  # pylint: disable=import-error
+
+
 
 def get_raw_lyrics(artist, title):
     """
@@ -22,40 +22,27 @@ def get_raw_lyrics(artist, title):
     """
 
     l_com_uid = "11118"
-    l_com_url = "https://www.stands4.com/services/v2/lyrics.php?uid=" + l_com_uid + "&tokenid=" + L_COM_KEY + "&term="
-    
- 
+    l_com_url = "https://www.stands4.com/services/v2/lyrics.php?uid=" + l_com_uid
+    l_com_url += "&tokenid=" + L_COM_KEY + "&term="
     l_com_url += title.replace(" ", "%20").replace("’", "%27")
     l_com_url += "&artist=" + artist.replace("’", "%27").replace(" ", "%20")
-
     l_com_url += "&format=json"
-
     print("L_COM_URL: " + l_com_url)
-    
     l_com_response = requests.get(l_com_url).json()
     if l_com_response == {'error': 'Daily Usage Exceeded'}:
         return "FAIL"
-
     print(l_com_response)
-
     lyrics_link = l_com_response["result"][0]["song-link"]
     print("LYRICS LINK FOR " + title + " BY " + artist + ": " + lyrics_link)
-
     url = lyrics_link
-    try:
-        url = url.replace(" ", "%20")
-        url = re.sub(r'[^a-zA-Z0-9./:]', "", url)
-        source = urlopen(url).read()
-        soup = BeautifulSoup(source, "html.parser")
-        for ele in soup.find_all("pre"):
-            if ele.get_text():
-                return ele.get_text()
-            else:
-                return " "
-    except:
-        print("fail")
-        return ""
-    
+    url = url.replace(" ", "%20")
+    url = re.sub(r'[^a-zA-Z0-9./:]', "", url)
+    source = urlopen(url).read()
+    soup = BeautifulSoup(source, "html.parser")
+    for ele in soup.find_all("pre"):
+        if ele.get_text():
+            return ele.get_text()
+        return " "
     return " "
 
 
@@ -77,18 +64,18 @@ def _get_artist_songs(artist_id):
     page = 1
     songs = []
     request = genius.artist_songs(artist_id,
-                                sort='popularity',
-                                per_page=50,
-                                page=1)
+                                  sort='popularity',
+                                  per_page=50,
+                                  page=1)
     while request["songs"]:
         for song in request["songs"]:
             songs.append(song["title"])
             # print(song["title"])
         page += 1
         request = genius.artist_songs(artist_id,
-                                sort='popularity',
-                                per_page=50,
-                                page=page)
+                                      sort='popularity',
+                                      per_page=50,
+                                      page=page)
 
     return songs
 
@@ -116,14 +103,15 @@ def get_titles(artist):
             song_file.write(song + "\n")
     return songs
 
-def get_song_lyrics(artist, index = 0):
+
+def get_song_lyrics(artist, index=0):
     """
     gets lyrics of a song
     """
     titles = []
     if "song_titles.txt" in os.listdir("."):
         with open("song_titles.txt", "r") as song_file:
-            titles = song_file.readlines()    
+            titles = song_file.readlines()
     else:
         titles = get_titles(artist)
 
@@ -139,7 +127,7 @@ def get_song_lyrics(artist, index = 0):
                     break
                 lyrics_file.write(lyrics)
                 count += 1
-            except:
+            except KeyError:
                 print(title)
                 continue
 
@@ -152,8 +140,8 @@ if __name__ == "__main__":
 
     # code to actually get lyrics
 
-    start_idx = 1
-    get_song_lyrics("Drake", start_idx)
+    START_IDX = 1
+    get_song_lyrics("Drake", START_IDX)
 
 
     # code to remove empty files
--- hello_world.py
+++
@@ -1,5 +0,0 @@
-"""Stupid ass main function"""
-def main():
-    """Main function to run the program.
-    No parameters needed, no return value. Prints dummy text to the console."""
-    print("Mad ting init")
